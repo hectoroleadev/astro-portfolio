6c3c06f662fd9d242254aef2bd82e9c138d4ff3b
Hector Olea <imacako@gmail.com>
2024-08-06 13:40:30 -0600

    fixed dark mode theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,18 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'selector',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeIn 1s ease-in-out',
+      },
+
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 };
